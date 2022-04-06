248ac75c8915ffc5dcf6b9f8c0dbd27ca806fedf
William Hagan <williamhagan1728@yahoo.com>
2022-04-05 18:12:54 -0700

    clean up files and import axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [songs, setSongs] = useState([]);
+
+  const getSongs = async () => {
+    try {
+      let response = await axios.get(
+        "http://www.devcodecampmusiclibrary.com/api/music"
+      );
+      console.log(response.data);
+      setSongs(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  <button onClick={getSongs}>click</button>
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  
+  </div>
+);
 }
+
+
+
 
 export default App;
